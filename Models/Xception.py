9875b6fcda431463fbbd243570ce6d7cec66bac8
flame0320 <s10159024@gmail.com>
2018-10-15 14:59:23 +0800

    Add files via upload
    整合4

---
+++ Models/Xception.py
@@ -0,0 +1,85 @@
+from tensorflow.keras.models import Model, Input
+from tensorflow.keras.layers import Conv2D, SeparableConv2D, Dense, Dropout, Flatten, BatchNormalization, add, Activation
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D, GlobalAveragePooling2D
+from ILBPLayer import MyLayer
+from CNN_Module import bn_relu, conv_bn_relu
+def GetMdl( ClassNum ):
+
+    input = Input(shape=(100, 100, 2))
+
+    x = conv_bn_relu(filters=32, kernel_size=(3, 3), strides=(2, 2))(input)
+    x = conv_bn_relu(filters=64, kernel_size=(3, 3))(x)
+
+    residual = Conv2D(128, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = bn_relu(x)
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = add([x, residual])
+
+    residual = Conv2D(256, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = add([x, residual])
+
+    residual = Conv2D(728, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)  
+    x = Activation('relu')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = add([x, residual])
+
+    for i in range(8):
+        residual = x
+
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = add([x, residual])
+
+    residual = Conv2D(1024, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(1024, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = add([x, residual])
+
+    x = SeparableConv2D(1536, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(2048, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = GlobalAveragePooling2D()(x)
+    output = Dense(ClassNum, activation='softmax')(x)
+
+    model = Model(inputs=input, outputs=output)
+
+    return model
